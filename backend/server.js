e38c60eb209b48409bf8e950bea33ec92814eca7
straxestv <eduardgamerx66@gmail.com>
2023-01-22 22:59:53 -0600

    m

--- backend/server.js
+++ backend/server.js
@@ -49,7 +49,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(`${__dirname}/../frontend/public/index.html`));
 });
 
-
-app.listen(config.PORT, () => {
-  console.log('Server started at http://localhost:${port}');
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
 });
